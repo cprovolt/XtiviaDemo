26a18ca21c984811cba1bb015217dc409073324d
Chris <cprovolt@yahoo.com>
2015-06-30 10:16:08 -0600

    All files from initial code push

---
+++ Database_Failover_Demo/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Database_Failover_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbStatus = new System.Windows.Forms.PictureBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timerCountdown = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ssStatus = new System.Windows.Forms.StatusStrip();
+            this.tssLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tssStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ssStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(15, 97);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(241, 22);
+            this.txtDatabase.TabIndex = 0;
+            this.txtDatabase.Text = "localhost";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(15, 143);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(241, 22);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "Uptime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(15, 189);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(241, 22);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.BackColor = System.Drawing.Color.Red;
+            this.pbStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbStatus.Location = new System.Drawing.Point(274, 12);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(112, 197);
+            this.pbStatus.TabIndex = 6;
+            this.pbStatus.TabStop = false;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(144, 231);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(112, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(274, 231);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timerCountdown
+            // 
+            this.timerCountdown.Interval = 5000;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 57);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ssStatus
+            // 
+            this.ssStatus.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ssStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssLabel,
+            this.tssStatus});
+            this.ssStatus.Location = new System.Drawing.Point(0, 259);
+            this.ssStatus.Name = "ssStatus";
+            this.ssStatus.Size = new System.Drawing.Size(398, 25);
+            this.ssStatus.TabIndex = 9;
+            this.ssStatus.Text = "statusStrip1";
+            // 
+            // tssLabel
+            // 
+            this.tssLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.tssLabel.Name = "tssLabel";
+            this.tssLabel.Size = new System.Drawing.Size(57, 20);
+            this.tssLabel.Text = "Status:";
+            // 
+            // tssStatus
+            // 
+            this.tssStatus.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tssStatus.MergeAction = System.Windows.Forms.MergeAction.Replace;
+            this.tssStatus.Name = "tssStatus";
+            this.tssStatus.Size = new System.Drawing.Size(0, 20);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(398, 284);
+            this.Controls.Add(this.ssStatus);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.pbStatus);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "XTIVIA Database Failover Demo";
+            
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ssStatus.ResumeLayout(false);
+            this.ssStatus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pbStatus;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timerCountdown;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip ssStatus;
+        private System.Windows.Forms.ToolStripStatusLabel tssLabel;
+        private System.Windows.Forms.ToolStripStatusLabel tssStatus;
+    }
+}
+
---
+++ Database_Failover_Demo/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Database_Failover_Demo
+{
+    public partial class Form1 : Form
+    {
+        private bool Connected = false;
+        private SqlConnection sqlconn;
+        private String conn;
+              
+        private int cooldown;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.cooldown = 1000;
+            this.timerCountdown.Interval = cooldown;
+            this.timerCountdown.Tick += TimerCountdown_Tick;
+            this.Change_Status(false);
+        }
+
+        private void TimerCountdown_Tick(object sender, EventArgs e)
+        {
+            sqlconn.Close();
+            sqlconn.Dispose();
+
+            try
+            {
+
+                this.sqlconn = new SqlConnection(conn);
+                this.sqlconn.Open();
+                this.pbStatus.BackColor = Color.Green;
+                this.Change_Status(true);
+
+            }
+            catch (Exception ex)
+            {
+                this.Connected = false;
+
+                this.pbStatus.BackColor = Color.Red;
+                this.Change_Status(false);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (Connected)
+            {
+                this.sqlconn.Close();
+                this.sqlconn.Dispose();
+                this.timerCountdown.Enabled = false;
+                this.txtUser.Focus();
+                this.btnConnect.Text = "Connect";
+                this.pbStatus.BackColor = Color.Red;
+                this.Change_Status(false);
+                return;
+            } /* end connected */
+
+            /* Determine if the text boxes are not blank for database and username */
+            if (this.txtDatabase.Text == "")
+            {
+                MessageBox.Show("There is no database name set. Please configure this value and try again");
+                this.txtDatabase.Focus();
+                return;
+            }
+            else if (this.txtUser.Text == "")
+            {
+                MessageBox.Show("There is no username name set. Please configure this value and try again");
+                this.txtUser.Focus();
+                return;
+            } /* end block */
+
+            /* connect to the database */
+            this.conn = "Data Source=" + this.txtDatabase.Text.ToString()
+                    + ";User ID="
+                    + this.txtUser.Text.ToString()
+                    + ";password=" + this.txtPassword.Text.ToString() + ";";
+            //MessageBox.Show(this.conn.ToString());
+
+            try
+            {
+                this.sqlconn = new SqlConnection(conn);
+                this.sqlconn.Open();
+
+                this.pbStatus.BackColor = Color.Green;
+                this.timerCountdown.Enabled = true;
+                this.Change_Status(true);
+
+                this.btnConnect.Text = "Disconnect";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+          } /* end method */
+
+       
+        private void Change_Status(bool value)
+        {
+            this.Connected = value;
+
+            if(Connected)
+            {
+                this.tssStatus.Text = "Connected to Database " + this.txtDatabase.Text + ".";
+            }
+            else
+            {
+                this.tssStatus.Text = "Not currently connected to a database.";
+            }
+        }
+
+     
+    }
+}
